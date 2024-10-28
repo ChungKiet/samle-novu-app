2f1db6bd376966affd535605d621f1db473d9b11
KietChung <chtkkiet@gmail.com>
2024-10-28 17:24:25 +0700

    update package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { NovuProvider, PopoverNotificationCenter, NotificationBell } from '@novu/notification-center';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NovuProvider subscriberId={'chtkkiet@gmail.com'} applicationIdentifier={'BNYiccnc_Qon'}>
+      <PopoverNotificationCenter>
+        {({ unseenCount }) => <NotificationBell unseenCount={unseenCount} />}
+      </PopoverNotificationCenter>
+    </NovuProvider>
   );
-}
+};
 
 export default App;
